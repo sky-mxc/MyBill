26207c802d9fa00e3cf9c7eb218528939b1bde5e
mxc <164443895@qq.com>
2016-10-26 00:28:32 +0800

    增加 相机拍照更换头像的功能

--- app/src/main/java/com/skymxc/mybill/MainActivity.java
+++ app/src/main/java/com/skymxc/mybill/MainActivity.java
@@ -1,9 +1,18 @@
 package com.skymxc.mybill;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.content.PermissionChecker;
 import android.support.v4.widget.SlidingPaneLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.PopupMenu;
@@ -15,6 +24,9 @@ import android.widget.PopupWindow;
 import android.widget.Toast;
 
 import com.skymxc.mybill.util.ImageViewPlus;
+import com.skymxc.mybill.util.PermissionUtil;
+
+import java.io.File;
 
 /**
  * 主窗体
@@ -23,10 +35,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     
     private static final String TAG = "MainActivity";
 
+    private static final int CAMERA_PERMISSION =0;
+    private static final int PHOTO_PERMISSION =1;
+    private static final int CAMERA = 2;
+    private static final int PHOTO = 3;
+    private static final int ZOOM = 4;
+
     private SlidingPaneLayout slidingPane;
     private Toolbar toolbar;
     private NavigationView navigation;
     private ImageViewPlus headImg;
+    private PopupWindow window;
+    private File mfile;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,16 +78,100 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.choose_camera:
                 Log.i(TAG, "onClick: choose_camera");
-                // TODO: 2016/10/25  相机拍照 创建头像 
+                // TODO: 2016/10/25  相机拍照 创建头像
+                if(window.isShowing()){
+                    window.dismiss();
+                }
+                callCamera();
                 break;
             case R.id.choose_photo:
                 Log.i(TAG, "onClick: choose_photo");
-                // TODO: 2016/10/25  相册选取图片 创建图片 
+                // TODO: 2016/10/25  相册选取图片 创建图片
+                if(window.isShowing()){
+                    window.dismiss();
+                }
                 break;
             case R.id.account:
                 showExit(v);
                 break;
         }
+    }
+
+    /**
+     * 调用相机拍照
+     */
+    private void callCamera() {
+        final String[] permissions = {"android.permission.CAMERA","android.permission.WRITE_EXTERNAL_STORAGE"};
+        //请求权限
+        if (PermissionUtil.checkVersion()){
+                if (!PermissionUtil.checkPermission(this,permissions)){
+                    if (!shouldShowRequestPermissionRationale(permissions[0])||!shouldShowRequestPermissionRationale(permissions[1])){
+                        new AlertDialog.Builder(this)
+                                .setTitle("权限说明")
+                                .setMessage("相机功能需要使用调用相机和内存卡读取的功能，这可能需要您的授权，请在稍后授权后使用")
+                                .setNegativeButton("确定", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                            requestPermissions(permissions, CAMERA_PERMISSION);
+                                        }
+
+                                    }
+                                })
+                                .setNeutralButton("算了，不拍了",null)
+                                .show();
+                    }else {
+                        //请求权限
+                        requestPermissions(permissions, CAMERA_PERMISSION);
+                    }
+                }else{
+                    invokeCamera();
+                }
+        }else{
+            invokeCamera();
+        }
+    }
+
+    /**
+     * 执行拍照
+     */
+    private void invokeCamera() {
+        String state = Environment.getExternalStorageState();
+        if(state.equals(Environment.MEDIA_MOUNTED)) {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            File file = new File(Environment.getExternalStorageDirectory(), "mybill");
+            if (!file.exists()) {
+                file.mkdir();
+            }
+            mfile = new File(file, "header.jpg");
+            if (mfile.exists()){
+                mfile.delete();
+            }
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(mfile));
+            startActivityForResult(intent, CAMERA);
+        }else{
+            Toast.makeText(this, "检测不到SD卡，无法使用", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * 打开系统图片裁剪功能
+     * @param uri
+     */
+    private void startPhotoZoom(Uri uri) {
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri,"image/*");
+        intent.putExtra("crop",true);
+        intent.putExtra("aspectX",1);
+        intent.putExtra("aspectY",1);
+        intent.putExtra("outputX",200);
+        intent.putExtra("outputY",200);
+        intent.putExtra("scale",true); //黑边
+        intent.putExtra("scaleUpIfNeeded",true); //黑边
+        intent.putExtra("return-data",true);
+        intent.putExtra("noFaceDetection",true);
+        startActivityForResult(intent,ZOOM);
+
     }
 
     /**
@@ -90,7 +195,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         View view = getLayoutInflater().inflate(R.layout.show_choose_headimg,null);
         view.findViewById(R.id.choose_camera).setOnClickListener(this);
         view.findViewById(R.id.choose_photo).setOnClickListener(this);
-        PopupWindow window = new PopupWindow(view);
+         window = new PopupWindow(view);
         window.setHeight((int) getResources().getDimension(R.dimen.choose_head_img_height));
         window.setWidth((int) getResources().getDimension(R.dimen.choose_head_img_width));
         window.setFocusable(true);
@@ -128,4 +233,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             return true;
         }
     };
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case CAMERA_PERMISSION:
+                if (PermissionChecker.PERMISSION_GRANTED==grantResults[0] && PermissionChecker.PERMISSION_GRANTED==grantResults[1]){
+                    //授权成功
+                    invokeCamera();
+                }else{
+                    //失败
+                    Toast.makeText(this, "授权失败，无法使用相机功能", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK && data != null){
+         switch (requestCode){
+             case CAMERA:
+                 //调用裁剪功能
+                 startPhotoZoom(Uri.fromFile(mfile));
+                 break;
+             case ZOOM:
+                 Bitmap bmp = data.getParcelableExtra("data");
+                 headImg.setImageBitmap(bmp);
+                 break;
+         }
+        }
+    }
 }
